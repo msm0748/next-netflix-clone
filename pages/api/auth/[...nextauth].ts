f2de395aa149478bf7fba557f1c4dc52a150f099
Moon <msm0748@gmail.com>
2023-05-17 11:23:56 +0900

    feat: Implemented Google/GitHub login feature

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,24 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
+
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import prismadb from "@/lib/prismadb";
 
 export const authOptions: AuthOptions = {
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || "",
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
         Credentials({
             id: "credentials",
             name: "Credentials",
@@ -47,6 +61,7 @@ export const authOptions: AuthOptions = {
         signIn: "/auth",
     },
     debug: process.env.NODE_ENV === "development",
+    adapter: PrismaAdapter(prismadb),
     session: { strategy: "jwt" },
     jwt: {
         secret: process.env.NEXTAUTH_JWT_SECRET,
